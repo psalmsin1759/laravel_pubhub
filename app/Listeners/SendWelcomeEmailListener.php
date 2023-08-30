c29412984e4095dfe0c0ed4ee21b0ca5ea50928f
psalmsin1759 <samson_ude@yahoo.com>
2023-08-31 00:53:04 +0100

    service account updated

--- app/Listeners/SendWelcomeEmailListener.php
+++ app/Listeners/SendWelcomeEmailListener.php
@@ -5,6 +5,7 @@ namespace App\Listeners;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 
+use Google\Cloud\Core\ServiceBuilder;
 use Google\Cloud\PubSub\PubSubClient;
 use App\Events\SendWelcomeEmail;
 
@@ -23,10 +24,17 @@ class SendWelcomeEmailListener
      */
     public function handle(SendWelcomeEmail $event)
     {
-        $pubsub = new PubSubClient([
-            'projectId' => env('GOOGLE_CLOUD_PROJECT_ID'),
+        $serviceB = new ServiceBuilder([
+            'credentials' => json_decode(env('GOOGLE_CLOUD_CONFIG'), true),
         ]);
 
+        $pubsub = $serviceB->pubsub();
+
+       
+       /*  $pubsub = new PubSubClient([
+            'projectId' => env('GOOGLE_CLOUD_PROJECT_ID'),
+        ]);
+ */
         $subscriptionName = env('PUBSUB_SUBSCRIPTION_NAME');
 
         $subscription = $pubsub->subscription($subscriptionName);
